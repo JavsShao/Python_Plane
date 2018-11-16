11121309ca39d9f880f0486f9987b3cda13f7b4a
hamdi <javs_shao@163.com>
2018-11-16 21:32:57 +0800

    显示游戏背景

---
+++ run.py
@@ -0,0 +1,29 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+
+SCREEN_WIDTH = 400
+SCREEN_HIGHT = 800
+
+# 初始化游戏
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIGHT))
+pygame.display.set_caption('飞机大战')
+
+# 载入背景图
+background = pygame.image.load('resources/image/background.png')
+
+while True:
+    # 绘制背景
+    screen.fill(0)
+    screen.blit(background, (0,0))
+
+    # 更新屏幕
+    pygame.display.update()
+
+    # 处理游戏退出
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
